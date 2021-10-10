1c35c695b590545bf63da09d2e911152e9c6c7e2
Kayla Morris <moreiskay14@gmail.com>
2021-10-09 20:23:58 -0700

    started on workout schema

--- models/workout.js
+++ models/workout.js
@@ -1,6 +1,36 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-    
-})
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+      },
+      name: {
+          type: String,
+      },
+      duration: {
+          type: Number
+      },
+      weight: {
+          type: Number,
+      },
+      reps: {
+          type: Number,
+      },
+      sets: {
+          type: Number,
+      },
+      distance: {
+          type: Number,
+      }
+    },
+  ],
+});
+
+// workoutSchema.virtual('')
