e8e02f490f139a8049f1ef2797b07f0c16ece5e8
Kayla Morris <moreiskay14@gmail.com>
2021-10-15 20:14:51 -0700

    Added save workout capabilities put&post routes

--- controllers/api/workoutRoutes.js
+++ controllers/api/workoutRoutes.js
@@ -1,23 +1,44 @@
 const router = require("express").Router();
-
 const { Workout } = require("../../models");
 
 router.get("/", (req, res) => {
-    Workout.aggregate([{ 
-        $addFields:{
-            totalDuration: { $sum: "$exercises.duration" }
-        }
-    }])
-    .then( workouts => {
-        res.json(workouts);
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: { $sum: "$exercises.duration" },
+      },
+    },
+  ])
+    .then((workouts) => {
+      res.json(workouts);
     })
-    .catch(err => {
-        res.json(err);
-    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 router.post("/", (req, res) => {
+  Workout.create(req.body)
+    .then((workoutData) => {
+      res.json(workoutData);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
-})
+router.put("/:id", (req, res) => {
+    Workout.updateOne({ 
+        _id: req.params.id 
+    }, {
+        $push: { exercises: req.body }
+    })
+    .then((workoutData) => {
+            res.json(workoutData);
+    })
+    .catch((err) => {
+            res.json(err);
+    });
+});
 
 module.exports = router;
